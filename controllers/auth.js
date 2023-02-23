8baa29fc1d2e3e69f3dfdf66d683d52779c20fe6
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-02-23 16:10:13 -0500

    create jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async(req, res = response ) => {
 
@@ -22,11 +23,15 @@ const createUser = async(req, res = response ) => {
     user.password = bcrypt.hashSync( password, salt );
 
     await user.save();
+
+    // GenerarJWT
+    const token = await generateJWT( user.id, user.name);
   
     res.status(201).json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     })
   } catch (error) {
     res.status(500).json({
@@ -60,10 +65,14 @@ const loginUser = async(req, res = response) => {
       });
     }
 
+      // GenerarJWT
+      const token = await generateJWT( user.id, user.name);
+
     res.json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     })
     
   } catch (error) {
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,22 @@
-const jwt = required('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const generateJWT = () => {
+const generateJWT = ( uid, name ) => {
+
+  return new Promise( (resolve, reject ) => {
+    
+    const payload = { uid, name };
+
+    jwt.sign( payload, process.env.SECRET_JWT_SEED, {
+      expiresIn: '2h'
+    }, (err, token) => {
+      if ( err ) {
+        console.log(err);
+        reject('No se pudo generar el token')
+      }
+
+      resolve( token );
+    })
+  })
 
 }
 
